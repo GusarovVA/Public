68d42db516bbfe6114a9bb4584818ab641140232
GusarovVA <Feecks413@gmail.com>
2024-12-11 12:19:47 +0300

    Add files via upload

---
+++ Переменные и типы данных/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val age = 31
+    val name = "Vladimir"
+    val isStudent = true
+    val salary = 160.5
+    println("Возраст: $age год | Имя: $name | Является студентом: $isStudent | Текущая заработная плата: $salary тыс. руб.")
+
+}

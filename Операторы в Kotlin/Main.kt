f96fc4cb6543d407a8b48432c145eed539ce488d
GusarovVA <Feecks413@gmail.com>
2024-12-11 12:22:17 +0300

    Add files via upload

---
+++ Операторы в Kotlin/Main.kt
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+        // Переменные:
+        val a = 2
+        val b = 4
+        val pi = 3.14
+        val d = 5
+        val l = 1020
+        val A = 100
+        val B = 13
+        //Вычисления:
+        val P = 4 * a // 1
+        val S = a * a // 2
+        val S2 = a * b // 3
+        val P2 = 2 * (a + b) // 3.2
+        val L = pi * d // 4
+        val количество1 = l / 100
+        val количество2 = A / B
+        // Результат:
+        println("Pкв = $P | Sкв = $S | Pпр = $P2 | Sпр = $S2 | Lокр = $L | Количество полных метров $количество1 | Количество отрезков $количество2")
+    }
